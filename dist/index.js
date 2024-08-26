9466f5d4a6756de5e9148662f02343ac75342273
Chaslen2101 <Chaslen2101@gmail.com>
2024-08-26 17:38:15 +0300

    edit_04

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const settings_1 = require("./settings");
-const port = process.env.PORT || 3999;
-settings_1.app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
