d6b0e41d7db5beb1bf98805ae444dceca1f66101
Chaslen2101 <Chaslen2101@gmail.com>
2024-08-26 16:11:21 +0300

    adding start page

--- src/settings.ts
+++ src/settings.ts
@@ -3,11 +3,19 @@ import express from "express"
 import {VideosRouter} from "./Routers/videosRouter";
 import {httpStatus} from "./DB/db";
 import {DBClear} from "./Routers/ClearDataBaseRouter";
+import {Request, Response} from "express";
 export const app = express()
 
 
 app.use(bodyParser.json())
 app.use('/hometask_01/api/testing/all-data', DBClear)
 app.use('/hometask_01/api/videos', VideosRouter)
+app.get('/', (req: Request, res: Response) => {
+
+    res
+        .status(httpStatus.NO_CONTENT_204)
+        .json("verison_01_01")
+
+})
 
 
